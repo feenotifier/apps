f367445fdbfebbd0b5567a64bfc5bfddc1184dbb
Ashish Chaubey <ashishchaubey866@gmail.com>
2021-09-20 01:04:36 +0530

    first call to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:apps/sections/login/landing.dart';
-import 'package:apps/sections/signup/landing.dart';
 import 'package:apps/sections/start/splash.dart';
 import 'package:flutter/material.dart';
 
@@ -20,36 +18,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     // return SignupLandingScreen();
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => SignupLandingScreen()));
-                },
-                child: Text("Test SignUp Screen"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => LoginLandingScreen()));
-                },
-                child: Text("Test Login Screen"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => SplashScreen()));
-                },
-                child: Text("Test Splash Screen"),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: SplashScreen(),
     );
   }
 }
