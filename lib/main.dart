5625224b3b34ce590c740adacfff40dda8f93e71
Ashish Chaubey <ashishchaubey866@gmail.com>
2021-09-19 20:52:12 +0530

    add splash screen and local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:apps/sections/login/landing.dart';
 import 'package:apps/sections/signup/landing.dart';
+import 'package:apps/sections/start/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,12 +22,31 @@ class _MyAppState extends State<MyApp> {
     return Scaffold(
       body: Container(
         child: Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => SignupLandingScreen()));
-            },
-            child: Text("Test SignUp Screen"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => SignupLandingScreen()));
+                },
+                child: Text("Test SignUp Screen"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => LoginLandingScreen()));
+                },
+                child: Text("Test Login Screen"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => SplashScreen()));
+                },
+                child: Text("Test Splash Screen"),
+              ),
+            ],
           ),
         ),
       ),
