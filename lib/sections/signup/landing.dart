aa22e729e2abd2a4417938b52ac5510734ed7a24
Ayush Anil Bharsakle <bharsakleaa@gmail.com>
2021-10-17 18:04:45 +0530

    Signup screen rederflex overflow

--- lib/sections/signup/landing.dart
+++ lib/sections/signup/landing.dart
@@ -74,210 +74,232 @@ class _SignupLandingScreenState extends State<SignupLandingScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       backgroundColor: Color(0xFFFD6F96),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(
-                Distance_Unit * 4,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FadeTransition(
-                    opacity: _animationController,
-                    child: SlideTransition(
-                      position: _arrowSlideAnimation,
-                      child: Container(
-                        padding: EdgeInsets.only(
-                          left: Distance_Unit * 2,
-                        ),
-                        height: Distance_Unit * 10,
-                        width: Distance_Unit * 10,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(
-                            Distance_Unit * 12,
-                          ),
-                        ),
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: Color(0xFF6F69AC),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: Distance_Unit * 12,
-                  ),
-                  SlideTransition(
-                    position: Tween<Offset>(
-                      begin: Offset(0, 0.3),
-                      end: Offset.zero,
-                    ).animate(
-                      CurvedAnimation(
-                        parent: _animationController,
-                        curve: Curves.easeIn,
-                      ),
-                    ),
-                    child: Container(
-                      child: Text(
-                        "Let's Sign up",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 30,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Spacer(),
-            FadeTransition(
-              opacity: _animationController,
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: FadeTransition(
+            opacity: _animationController,
+            child: SlideTransition(
+              position: _arrowSlideAnimation,
               child: Container(
+                padding: EdgeInsets.only(
+                  left: Distance_Unit * 2,
+                ),
+                height: Distance_Unit * 10,
+                width: Distance_Unit * 10,
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(32),
-                    topRight: Radius.circular(32),
+                  borderRadius: BorderRadius.circular(
+                    Distance_Unit * 12,
                   ),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: Distance_Unit * 4,
-                    right: Distance_Unit * 4,
-                    bottom: Distance_Unit * 4,
-                    top: Distance_Unit * 15,
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Color(0xFF6F69AC),
+                ),
+              ),
+            ),
+          ),
+        ),
+        backgroundColor: Color(0xFFFD6F96),
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.86,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(
+                    Distance_Unit * 4,
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      if (step == "firstStep") ...[
-                        GenericTextField(
-                          labelText: "First Name",
-                          keyboardType: TextInputType.emailAddress,
-                          prefixIconName: Icons.person,
-                          prefixIconColor: Color(0xFF6F69AC),
-                          borderColor: Color(0xFF6F69AC),
-                          focusBorderColor: Color(0xFFFD6F96),
-                          labelTextColor: Color(0xFF6F69AC),
-                          onChanged: (val) {
-                            email = val;
-                          },
-                        ),
-                        SizedBox(
-                          height: Distance_Unit * 4,
-                        ),
-                        GenericTextField(
-                          labelText: "Last Name",
-                          keyboardType: TextInputType.emailAddress,
-                          prefixIconName: Icons.person,
-                          prefixIconColor: Color(0xFF6F69AC),
-                          borderColor: Color(0xFF6F69AC),
-                          focusBorderColor: Color(0xFFFD6F96),
-                          labelTextColor: Color(0xFF6F69AC),
-                          onChanged: (val) {
-                            email = val;
-                          },
-                        ),
-                        SizedBox(
-                          height: Distance_Unit * 4,
-                        ),
-                        GenericTextField(
-                          labelText: "Mobile Number",
-                          keyboardType: TextInputType.emailAddress,
-                          prefixIconName: Icons.phone,
-                          prefixIconColor: Color(0xFF6F69AC),
-                          borderColor: Color(0xFF6F69AC),
-                          focusBorderColor: Color(0xFFFD6F96),
-                          labelTextColor: Color(0xFF6F69AC),
-                          onChanged: (val) {
-                            email = val;
-                          },
-                        ),
-                      ],
-                      if (step == "finalStep") ...[
-                        GenericTextField(
-                          labelText: "Password",
-                          keyboardType: TextInputType.emailAddress,
-                          prefixIconName: Icons.remove_red_eye,
-                          prefixIconColor: Color(0xFF6F69AC),
-                          borderColor: Color(0xFF6F69AC),
-                          focusBorderColor: Color(0xFFFD6F96),
-                          labelTextColor: Color(0xFF6F69AC),
-                          onChanged: (val) {
-                            email = val;
-                          },
-                        ),
-                        SizedBox(
-                          height: Distance_Unit * 4,
-                        ),
-                        GenericTextField(
-                          labelText: "Confirm Password",
-                          keyboardType: TextInputType.emailAddress,
-                          prefixIconName: Icons.password,
-                          prefixIconColor: Color(0xFF6F69AC),
-                          borderColor: Color(0xFF6F69AC),
-                          focusBorderColor: Color(0xFFFD6F96),
-                          labelTextColor: Color(0xFF6F69AC),
-                          onChanged: (val) {
-                            email = val;
-                          },
-                        ),
-                      ],
                       SizedBox(
-                        height: Distance_Unit * 4,
+                        height: Distance_Unit * 12,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: Distance_Unit * 20,
+                      SlideTransition(
+                        position: Tween<Offset>(
+                          begin: Offset(0, 0.3),
+                          end: Offset.zero,
+                        ).animate(
+                          CurvedAnimation(
+                            parent: _animationController,
+                            curve: Curves.easeIn,
+                          ),
                         ),
-                        child: GenericButtons(
-                          title: step == "firstStep" ? "Next" : "Submit",
-                          backgroundColor: Color(0xFFFD6F96),
-                          textColor: Colors.white,
-                          suffixIconColor: Colors.white,
-                          suffixIconData: step == "firstStep"
-                              ? Icons.arrow_forward_ios
-                              : null,
-                          onTap: () {
-                            // http.Response response = await createAlbum(
-                            //   email,
-                            //   password,
-                            // );
-                            // Map<String, dynamic> map =
-                            //     json.decode(response.body);
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //     builder: (_) => HomePage(
-                            //         // userId: map['data']['userId'],
-                            //         ),
-                            //   ),
-                            // );
-
-                            if (step == "firstStep") {
-                              setState(() {
-                                step = "finalStep";
-                              });
-                            }
-                            if (step == "finalStep") {
-                              print("Sign up successful");
-                            }
-                          },
+                        child: Container(
+                          child: Text(
+                            "Let's Sign up",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 30,
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
+                Spacer(),
+                Column(
+                  children: [
+                    FadeTransition(
+                      opacity: _animationController,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(32),
+                            topRight: Radius.circular(32),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: Distance_Unit * 4,
+                            right: Distance_Unit * 4,
+                            bottom: Distance_Unit * 4,
+                            top: Distance_Unit * 15,
+                          ),
+                          child: Container(
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  if (step == "firstStep") ...[
+                                    GenericTextField(
+                                      labelText: "First Name",
+                                      keyboardType: TextInputType.emailAddress,
+                                      prefixIconName: Icons.person,
+                                      prefixIconColor: Color(0xFF6F69AC),
+                                      borderColor: Color(0xFF6F69AC),
+                                      focusBorderColor: Color(0xFFFD6F96),
+                                      labelTextColor: Color(0xFF6F69AC),
+                                      onChanged: (val) {
+                                        email = val;
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: Distance_Unit * 4,
+                                    ),
+                                    GenericTextField(
+                                      labelText: "Last Name",
+                                      keyboardType: TextInputType.emailAddress,
+                                      prefixIconName: Icons.person,
+                                      prefixIconColor: Color(0xFF6F69AC),
+                                      borderColor: Color(0xFF6F69AC),
+                                      focusBorderColor: Color(0xFFFD6F96),
+                                      labelTextColor: Color(0xFF6F69AC),
+                                      onChanged: (val) {
+                                        email = val;
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: Distance_Unit * 4,
+                                    ),
+                                    GenericTextField(
+                                      labelText: "Mobile Number",
+                                      keyboardType: TextInputType.emailAddress,
+                                      prefixIconName: Icons.phone,
+                                      prefixIconColor: Color(0xFF6F69AC),
+                                      borderColor: Color(0xFF6F69AC),
+                                      focusBorderColor: Color(0xFFFD6F96),
+                                      labelTextColor: Color(0xFF6F69AC),
+                                      onChanged: (val) {
+                                        email = val;
+                                      },
+                                    ),
+                                  ],
+                                  if (step == "finalStep") ...[
+                                    GenericTextField(
+                                      labelText: "Password",
+                                      keyboardType: TextInputType.emailAddress,
+                                      prefixIconName: Icons.remove_red_eye,
+                                      prefixIconColor: Color(0xFF6F69AC),
+                                      borderColor: Color(0xFF6F69AC),
+                                      focusBorderColor: Color(0xFFFD6F96),
+                                      labelTextColor: Color(0xFF6F69AC),
+                                      onChanged: (val) {
+                                        email = val;
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: Distance_Unit * 4,
+                                    ),
+                                    GenericTextField(
+                                      labelText: "Confirm Password",
+                                      keyboardType: TextInputType.emailAddress,
+                                      prefixIconName: Icons.password,
+                                      prefixIconColor: Color(0xFF6F69AC),
+                                      borderColor: Color(0xFF6F69AC),
+                                      focusBorderColor: Color(0xFFFD6F96),
+                                      labelTextColor: Color(0xFF6F69AC),
+                                      onChanged: (val) {
+                                        email = val;
+                                      },
+                                    ),
+                                  ],
+                                  SizedBox(
+                                    height: Distance_Unit * 4,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                      top: Distance_Unit * 20,
+                                    ),
+                                    child: GenericButtons(
+                                      title: step == "firstStep"
+                                          ? "Next"
+                                          : "Submit",
+                                      backgroundColor: Color(0xFFFD6F96),
+                                      textColor: Colors.white,
+                                      suffixIconColor: Colors.white,
+                                      suffixIconData: step == "firstStep"
+                                          ? Icons.arrow_forward_ios
+                                          : null,
+                                      onTap: () {
+                                        // http.Response response = await createAlbum(
+                                        //   email,
+                                        //   password,
+                                        // );
+                                        // Map<String, dynamic> map =
+                                        //     json.decode(response.body);
+                                        // Navigator.push(
+                                        //   context,
+                                        //   MaterialPageRoute(
+                                        //     builder: (_) => HomePage(
+                                        //         // userId: map['data']['userId'],
+                                        //         ),
+                                        //   ),
+                                        // );
+
+                                        if (step == "firstStep") {
+                                          setState(() {
+                                            step = "finalStep";
+                                          });
+                                        }
+                                        if (step == "finalStep") {
+                                          print("Sign up successful");
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
