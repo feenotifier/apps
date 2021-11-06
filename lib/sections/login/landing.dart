948fcee626e18c832620ff28fe9c8a36c4d2112c
Ashish Chaubey <ashishchaubey866@gmail.com>
2021-11-06 22:05:04 +0530

    [Fix] Fix issue for exiting app (#56)
    * add files for api call and validations
    
    * fix exit app issue

--- lib/sections/login/landing.dart
+++ lib/sections/login/landing.dart
@@ -5,6 +5,7 @@ import 'package:apps/sections/shared/otp_verification.dart';
 import 'package:apps/services/api.dart';
 import 'package:apps/utility/validation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 class LoginLandingScreen extends StatefulWidget {
@@ -59,122 +60,127 @@ class _LoginLandingScreenState extends State<LoginLandingScreen>
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Color.fromRGBO(18, 18, 18, 60),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(
-                Distance_Unit * 4,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FadeTransition(
-                    opacity: _animationController,
-                    child: SlideTransition(
-                      position: _arrowSlideAnimation,
-                    ),
-                  ),
-                  SizedBox(
-                    height: Distance_Unit * 7,
-                  ),
-                  SlideTransition(
-                    position: Tween<Offset>(
-                      begin: Offset(0, 0.3),
-                      end: Offset.zero,
-                    ).animate(
-                      CurvedAnimation(
-                        parent: _animationController,
-                        curve: Curves.easeIn,
-                      ),
-                    ),
-                    child: Container(
-                      child: Text(
-                        "Fee Notifier",
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 198, 137, 40),
-                          fontWeight: FontWeight.w600,
-                          fontSize: 40,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Spacer(),
-            FadeTransition(
-              opacity: _animationController,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(45, 45, 45, 100),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
+      body: WillPopScope(
+        onWillPop: () async {
+          return SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+        },
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(
+                  Distance_Unit * 4,
                 ),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: Distance_Unit * 4,
-                    right: Distance_Unit * 4,
-                    bottom: Distance_Unit * 4,
-                    top: Distance_Unit * 10,
-                  ),
-                  child: Column(
-                    children: [
-                      GenericTextField(
-                        labelText: "Email",
-                        keyboardType: TextInputType.emailAddress,
-                        prefixIconName: Icons.mail,
-                        prefixIconColor: Color.fromRGBO(209, 209, 209, 20),
-                        borderColor: Color.fromRGBO(255, 198, 137, 40),
-                        focusBorderColor: Color.fromRGBO(255, 198, 137, 40),
-                        labelTextColor: Color.fromRGBO(209, 209, 209, 10),
-                        textColor: Colors.white,
-                        onChanged: (val) {
-                          email = val;
-                        },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FadeTransition(
+                      opacity: _animationController,
+                      child: SlideTransition(
+                        position: _arrowSlideAnimation,
                       ),
-                      SizedBox(
-                        height: Distance_Unit * 2,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: Distance_Unit * 90,
+                    ),
+                    SizedBox(
+                      height: Distance_Unit * 7,
+                    ),
+                    SlideTransition(
+                      position: Tween<Offset>(
+                        begin: Offset(0, 0.3),
+                        end: Offset.zero,
+                      ).animate(
+                        CurvedAnimation(
+                          parent: _animationController,
+                          curve: Curves.easeIn,
                         ),
-                        child: GenericButtons(
-                          title: "Next",
-                          backgroundColor: Color.fromRGBO(255, 198, 137, 40),
-                          textColor: Colors.black54,
-                          suffixIconColor: Colors.black54,
-                          suffixIconData: Icons.arrow_forward_ios,
-                          onTap: () async {
-                            if (isEmailValid(email)) {
-                              http.Response response = await sendOTP(email);
-                              if (response.body != null &&
-                                  response.body.isNotEmpty)
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (_) => OtpVerificationScreen(
-                                      otp: response.body.toString(),
-                                      email: email,
-                                    ),
-                                  ),
-                                );
-                            } else {
-                              print("Not a valid email");
-                            }
+                      ),
+                      child: Container(
+                        child: Text(
+                          "Fee Notifier",
+                          style: TextStyle(
+                            color: Color.fromRGBO(255, 198, 137, 40),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              FadeTransition(
+                opacity: _animationController,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(45, 45, 45, 100),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: Distance_Unit * 4,
+                      right: Distance_Unit * 4,
+                      bottom: Distance_Unit * 4,
+                      top: Distance_Unit * 10,
+                    ),
+                    child: Column(
+                      children: [
+                        GenericTextField(
+                          labelText: "Email",
+                          keyboardType: TextInputType.emailAddress,
+                          prefixIconName: Icons.mail,
+                          prefixIconColor: Color.fromRGBO(209, 209, 209, 20),
+                          borderColor: Color.fromRGBO(255, 198, 137, 40),
+                          focusBorderColor: Color.fromRGBO(255, 198, 137, 40),
+                          labelTextColor: Color.fromRGBO(209, 209, 209, 10),
+                          textColor: Colors.white,
+                          onChanged: (val) {
+                            email = val;
                           },
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: Distance_Unit * 2,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: Distance_Unit * 90,
+                          ),
+                          child: GenericButtons(
+                            title: "Next",
+                            backgroundColor: Color.fromRGBO(255, 198, 137, 40),
+                            textColor: Colors.black54,
+                            suffixIconColor: Colors.black54,
+                            suffixIconData: Icons.arrow_forward_ios,
+                            onTap: () async {
+                              if (isEmailValid(email)) {
+                                http.Response response = await sendOTP(email);
+                                if (response.body != null &&
+                                    response.body.isNotEmpty)
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) => OtpVerificationScreen(
+                                        otp: response.body.toString(),
+                                        email: email,
+                                      ),
+                                    ),
+                                  );
+                              } else {
+                                print("Not a valid email");
+                              }
+                            },
+                          ),
+
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
